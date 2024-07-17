c961c24ce8d4f54defe3023b1dc0f95e4c886629
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2024-07-16 22:42:12 -0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
 
+import com.enums.CharacterClassType;
+import com.enums.CourseType;
+import com.enums.GenderType;
+import com.enums.RaceType;
+import com.model.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Player p1 = new Player("Jose", null, CourseType.COMPUTER_SCIENCE, "999999", GenderType.HOMEM, 21, "...");
+        p1.createCharacter("ZION", 10, GenderType.HOMEM, 20, CharacterClassType.GUERREIRO, RaceType.HUMANO, null, "...");
+        p1.createCharacter("Asterius", 10, GenderType.HOMEM, 34, CharacterClassType.MAGO, RaceType.ELFO, null, "...");
+        p1.createCharacter("Legolas", 10, GenderType.HOMEM, 200, CharacterClassType.GUERREIRO, RaceType.ELFO, null, "...");
+        p1.createCharacter("Basneria", 10, GenderType.MULHER, 120, CharacterClassType.CURANDEIRO, RaceType.ELFO, null, "...");
+        p1.printSheetList();
 
+        p1.deleteCharacter();
+        p1.printSheetList();
     }
 }
